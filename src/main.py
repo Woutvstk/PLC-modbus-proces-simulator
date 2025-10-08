11fac8e06df17b6d0c597f19827ba06b0d253fc8
Woutvstk <119763111+Woutvstk@users.noreply.github.com>
2025-10-09 00:14:37 +0200

    Add configuration and connect process with PlcCom trough main

--- src/main.py
+++ src/main.py
@@ -2,42 +2,121 @@ from processSim.tankSim import tankSim
 from plcCom.plcModBusTCP import plcModBusTCP
 from plcCom.plcS7 import plcS7
 import time
+from configuration import configuration
+
+
+"""Initialize configuration instance with default parameters"""
+config = configuration()
 
 """Initialize process0 object"""
 
 
-Protocol = "S7"   # of "ModBusTCP"
+process0 = tankSim("process0", 2000, 250, 135, 1000, 250, 21)
+process0.simStart()
 
-if Protocol == "ModBusTCP":
-    PlcCom = plcModBusTCP("192.168.111.80", 502)
-elif Protocol == "S7":
-    PlcCom = plcS7("192.168.111.80", 0, 1)
 
-PlcCom.connect()
+validPlcConnection: bool = False
+
+
+# remember at what time we started
+startTime = time.time()
+
+plcAnalogMax = 65536
+
+
+def mapValue(oldMin: int, oldMax: int, newMin: int, newMax: int, old: float) -> float:
+    return (old-oldMin)*(newMax-newMin)/(oldMax-oldMin)+newMin
+
+
+def sendPlcOutToProcessAndUi():
+    if (config.DQValveIn):  # if DQ valveIn = 1, ignore analog setpoint
+        process0.valveInOpenFraction = 1
+    else:
+        process0.valveInOpenFraction = mapValue(
+            0, plcAnalogMax, 0, 1, PlcCom.GetAO(config.AQValveInFraction))
+    if (config.DQValveOut):  # if DQ valveOut = 1, ignore analog setpoint
+        process0.valveOutOpenFraction = 1
+    else:
+        process0.valveOutOpenFraction = mapValue(
+            0, plcAnalogMax, 0, 1, PlcCom.GetAO(config.AQValveOutFraction))
+    if (config.DQHeater):  # if DQ heater = 1, ignore analog setpoint
+        process0.heaterPowerFraction = 1
+    else:
+        process0.heaterPowerFraction = PlcCom.GetAO(config.AQHeaterFraction)
+
+
+def sendProcessStatusToPlcInAndUi():
+    PlcCom.SetDI(config.DILevelSensorHigh,
+                 process0.digitalLevelSensorHighTriggered)
+    PlcCom.SetDI(config.DILevelSensorLow,
+                 process0.digitalLevelSensorLowTriggered)
+    PlcCom.SetAI(config.AILevelSensor, mapValue(
+        0, process0.tankVolume, 0, plcAnalogMax, process0.liquidVolume))
+    PlcCom.SetAI(config.AITemperatureSensor, mapValue(-50, 250,
+                 0, plcAnalogMax, process0.liquidTemperature))
 
 
 while True:
 
-    '''Examples of using the plcCom class'''
+    if (validPlcConnection == False):  # no valid connection -> try to connect
 
-    # 3️Set some digital inputs
-    print("Setting digital inputs...")
-    PlcCom.SetDI(0, 1)  # Set DI0 to 1 (ON)
-    PlcCom.SetDI(1, 0)  # Set DI1 to 0 (OFF)
+        # TODO: notify UI that there is no connection
+        # UI.validPlcConnection = False
+        # UI.update()
+        # TODO: wait until UI connect button is clicked
+        # if(UI.connectButtonClicked)
+        #   UI.connectButtonClicked = False
+        #
+        #   TODO: ask UI to update config
+        #   UI.updateConfig(config)
+        """"Initialize plc communication object"""
+        if config.plcProtocol == "ModBusTCP":
+            PlcCom = plcModBusTCP(config.plcIpAdress, config.plcPort)
+        elif config.plcProtocol == "S7":
+            # IP address, rack, slot (from HW settings)
+            PlcCom = plcS7(config.plcIpAdress, config.plcRack, config.plcSlot)
+        else:
+            print("Error: no valid plcProtocol")
 
-    # Set some analog inputs
-    print("Setting analog inputs...")
-    PlcCom.SetAI(0, 12345)  # Set AI0 (register 16) to 12345
-    PlcCom.SetAI(15, 30000)  # Set AI15 (register 17) to 30000
+        # TODO: implement PlcCom.isConnected() function. isConnected should return true if there is a valid connection
+        # if(PlcCom.isConnected()):
+        if (False):  # placeholder for line above
+            validPlcConnection = True
+            PlcCom.connect()
+            PlcCom.reset_registers()
+        else:
+            validPlcConnection = False
 
-    #  Read digital outputs (DO)
-    print("Reading digital outputs...")
-    do0 = PlcCom.GetDO(0)
-    do1 = PlcCom.GetDO(1)
+    elif (validPlcConnection == True):  # valid connection -> run main logic
 
-    #  Read analog outputs (AO)
-    print("Reading analog outputs...")
-    ao0 = PlcCom.GetAO(0)
-    ao1 = PlcCom.GetAO(1)
+        """
+        Check if PLC is still connected
+        """
+        # TODO: implement PlcCom.isConnected() function.
+        # if(PlcCom.isConnected() == False): #check if plc is still connected
+        if (False == False):  # placeholder for line above
+            validPlcConnection == False
+            continue
 
-    time.sleep(1.5)
+        """
+        Transfer data between modules
+        """
+        sendPlcOutToProcessAndUi()
+        sendProcessStatusToPlcInAndUi()
+
+        """
+        Check if a config/process update happened in UI and update config/process if needed
+        """
+        # TODO: implement UI.hasConfigUpdate() and UI.updateConfig(config)
+        # if(UI.hasConfigUpdate()):
+        #    UI.updateConfig(config)
+
+        # TODO: implement UI.hasProcessUpdate() and UI.updateProcess(process)
+        # if(UI.hasProcessUpdate()):
+        #    UI.updateProcess(process0)
+
+    # print out the current time since start and status
+    print(
+        f"Time: {int(time.time() - startTime)}, ValidConnection: {validPlcConnection}, Liquid level: {int(process0.liquidVolume)}, Liquid temp: {int(process0.liquidTemperature)}")
+    # throttle code
+    time.sleep(0.1)
