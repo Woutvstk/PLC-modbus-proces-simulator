cf631eedfc0699fe59cdd979174e0577e2863dba
JarneVDS <jarne.vanderstraeten12@gmail.com>
2025-11-11 10:34:45 +0100

    working state api V1
    saving working state before changing connect function

---
+++ Debug/plcSimAPIDebug.py
@@ -0,0 +1,163 @@
+import clr
+import os
+import time
+
+#connects to to the softbus of a Siemens PLC simulator via an API DLL
+
+# --- PAD NAAR DLL ---
+dll_path = r"D:\Eigen projecten\visual studio\DEV\PLC-modbus-proces-simulator\src\plcCom\Siemens.Simatic.Simulation.Runtime.Api.x64.dll"
+if not os.path.exists(dll_path):
+    raise FileNotFoundError(f"DLL niet gevonden: {dll_path}")
+
+clr.AddReference(dll_path)
+from Siemens.Simatic.Simulation.Runtime import SimulationRuntimeManager #type: ignore
+
+instance_name = "PLC1"
+
+class plcSimAPI:
+
+    """Class for communication with a Siemens S7 PLC simulator via Simatic.Simulation.Runtime API"""
+
+    def __init__(self):
+        """Initialize the PLC simulator interface with the given instance name"""
+        global manager , instance 
+        manager = SimulationRuntimeManager() 
+        instance = manager.RegisteredInstanceInfo 
+    
+    def connect(self, instance_name: str): 
+        """Create an interface to the specified PLC simulation instance Return true = connected, false = not connected""" 
+        for inst in instance: 
+            if inst.Name == instance_name: 
+                try: 
+                    self.simulation_instance = manager.CreateInterface(inst.ID) 
+                    print(f"Interface created for instance: {instance_name}") 
+                    return True 
+                except Exception:
+                    print(f"Fout bij het maken van de interface voor instantie:", instance_name)
+                    return False 
+                    print(f"Instance '{instance_name}' not found.")
+
+    def isConnected(self) -> bool:
+        """Check if the connection to the PLC simulator is alive"""
+        try:
+            if self.simulation_instance is not None:
+                return True
+            else:
+                print("No simulation instance connected.")
+                return False
+        except Exception as e:
+            print("Connection error:", e)
+            return False
+        
+    def Disconnect(self, instance):
+        """Disconnect from the PLC simulator instance"""
+        try:
+            if self.simulation_instance is not None:
+                self.simulation_instance = None
+                for inst in instance:
+                    self.simulation_instance = manager.DestroyInterface(inst.ID)
+                print("Disconnected from PLC simulator instance.")
+        except Exception as e:
+            print("Disconnection error:", e)
+
+    def SetDI(self, startByte:int, bit:int, value: int):
+        """
+        Set a digital input (DI) bit in the PLC input process image (E/I area).
+
+        byte: selects which input byte in the PLC is used, as defined by the GUI  
+        bit: bit position (0–7) within the selected byte  
+        value: True/False or 1/0 to set or clear the bit
+        """
+        if self.isConnected():
+            if startByte >= 0 and 0 <= bit < 8:
+                self.simulation_instance.InputArea.WriteBit(startByte,bit, bool(value))
+    
+    def GetDO(self,startbyte: int, bit: int):
+        """
+        Read a digital output (DO) bit from the PLC output process image (A/Q area).
+
+        byte: selects which output byte in the PLC is used, as defined by the GUI  
+        bit: bit position (0–7) within the selected byte
+        """
+        if self.isConnected():
+            if startbyte >= 0:
+                if 7 >= bit >= 0:
+                    data = bytes(1)
+                    data = self.simulation_instance.OutputArea.ReadBit(startbyte,bit)
+                    return int(data)
+            return -1
+
+
+    def SetAI(self,byte: int, value: int):
+        """
+        Set an analog input (AI) value as a 16-bit UNSIGNED INTEGER (0–65535) in the PLC input process image (E/I area).
+
+        byte: selects which input byte in the PLC is used, as defined by the GUI  
+        value: 0–65535 (word)
+        """
+        if self.isConnected():
+            buffer_AI = bytearray(2)
+            if byte >= 0:
+                if 0 <= value <= 65535:
+                    lowByte = value & 0xFF  # 0xFF = mask for one byte (0b11111111)
+                    highByte = (value >> 8) & 0xFF
+                    buffer_AI[0] = highByte
+                    buffer_AI[1] = lowByte
+                    self.simulation_instance.InputArea.WriteBytes(byte,2,buffer_AI)
+                    return int(value)
+                return -1
+            return -1
+
+
+    def GetAO(self, startByte: int):
+        """
+        Read an analog output (AO) value as a 16-bit SIGNED INTEGER (-32768–32767) from the PLC output process image (A/Q area).
+
+        byte: selects which output byte in the PLC is used, as defined by the GUI
+        """
+        if self.isConnected():
+            if startByte >= 0:
+                data = bytes(2)
+                data = self.simulation_instance.OutputArea.ReadBytes(startByte,2)
+                value = int.from_bytes(data, byteorder='big', signed = True)
+                return int(value)
+
+    def print_status(self):
+        if not self.isConnected():
+            print("Niet verbonden met PLC.")
+            return
+
+        di_vals, do_vals, ai_vals, ao_vals = [], [], [], []
+
+        # DI / DO (2 bytes = 16 bits)
+        for byte in range(0,2):
+            for bit in range(0,8):
+                di = self.simulation_instance.InputArea.ReadBit(byte, bit)
+                do = self.simulation_instance.OutputArea.ReadBit(byte, bit)
+                di_vals.append(f"{byte}.{bit}:{int(di)}")
+                do_vals.append(f"{byte}.{bit}:{int(do)}")
+
+        # AI / AO (16 kanalen = 2 bytes elk)
+        for i in range(2, 34, 2):
+            ai_data = self.simulation_instance.InputArea.ReadBytes(i, 2)
+            ao_data = self.simulation_instance.OutputArea.ReadBytes(i, 2)
+            ai_val = int.from_bytes(ai_data, byteorder="big", signed=False)
+            ao_val = int.from_bytes(ao_data, byteorder="big", signed=True)
+            ai_vals.append(f"{i}:{ai_val}")
+            ao_vals.append(f"{i}:{ao_val}")
+
+        print(f"DI: {' '.join(di_vals)}")
+        print(f"DO: {' '.join(do_vals)}")
+        print(f"AI: {' '.join(ai_vals)}")
+        print(f"AO: {' '.join(ao_vals)}")
+        print("-" * 90)
+
+simulator = plcSimAPI()
+
+while True:  
+    if simulator.isConnected() == False:
+        
+        simulator.connect("PLC1")
+    simulator.print_status()
+
+    time.sleep(1)    
---
+++ src/plcCom/PLCSimAPI.py
@@ -0,0 +1,148 @@
+import clr
+import os
+import sys
+
+#connects to to the softbus of a Siemens PLC simulator via an API DLL
+
+# --- PAD NAAR DLL ---
+dll_path = r"D:\Eigen projecten\visual studio\DEV\PLC-modbus-proces-simulator\src\plcCom\Siemens.Simatic.Simulation.Runtime.Api.x64.dll"
+if not os.path.exists(dll_path):
+    raise FileNotFoundError(f"DLL niet gevonden: {dll_path}")
+
+clr.AddReference(dll_path)
+from Siemens.Simatic.Simulation.Runtime import SimulationRuntimeManager #type: ignore
+
+# --- MANAGER MAKEN ---
+manager = SimulationRuntimeManager()
+
+# --- Property, geen methode ---
+instances = manager.RegisteredInstanceInfo  # <-- geen ()
+
+print("Beschikbare PLC-simulatie-instanties:")
+for i, inst in enumerate(instances):
+    # Gebruik f-string correct, met juiste case
+    print(f"Instantie={i}, Naam={inst.Name}, ID={inst.ID}")
+
+class plcSimAPI:
+
+    """Class for communication with a Siemens S7 PLC using Snap7"""
+
+    def __init__(self, ip: str, rack: int, slot: int, tcpport: int = 102):
+        """Initialize the PLC client with IP, rack, slot, and TCP port"""
+
+
+    def connect(self):
+        """Connect to the PLC, returns "true" if connected"""
+        try:
+            self.client.connect(self.ip, self.rack, self.slot, self.tcpport)
+            if self.client.get_connected():
+                print(
+                    f"Connected to S7 PLC at {self.ip}:{self.tcpport} (rack {self.rack}, slot {self.slot})")
+                return True
+            else:
+                print(f"Cannot connect to S7 PLC at {self.ip}")
+                return False
+        except Exception as e:
+            print("Connection error:", e)
+            return False
+
+    def disconnect(self):
+        """Disconnect from the PLC if the connection is active"""
+        if self.client.get_connected():
+            self.client.disconnect()
+
+    def isConnected(self) -> bool:
+        """check if the connection is alive, connected returns "true" """
+        if not self.client.get_connected():
+            print("Connection lost to the PLC!")
+            return False
+        else:
+            return True
+
+    def SetDI(self, byte: int, bit: int, value: int):
+        """
+        Set a digital input (DI) bit in the PLC input process image (E/I area).
+
+        byte: selects which input byte in the PLC is used, as defined by the GUI  
+        bit: bit position (0–7) within the selected byte  
+        value: True/False or 1/0 to set or clear the bit
+        """
+        if self.isConnected():
+            buffer_DI = bytearray(2)
+            if byte >= 0:
+                if 7 >= bit >= 0:
+                    if value:  # if the value is > 0
+                        # shift binary 1 by bit index, e.g. (1 << 3) = 00001000
+                        buffer_DI[0] |= (1 << bit)
+                    else:
+                        # invert bit mask, e.g. ~(1 << 3) = 11110111
+                        buffer_DI[0] &= ~(1 << bit)
+                    self.client.eb_write(start=byte, size=1, data=buffer_DI)
+                    return int(bool(value))
+            return -1
+
+    def GetDO(self, byte: int, bit: int):
+        """
+        Read a digital output (DO) bit from the PLC output process image (A/Q area).
+
+        byte: selects which output byte in the PLC is used, as defined by the GUI  
+        bit: bit position (0–7) within the selected byte
+        """
+        if self.isConnected():
+            if byte >= 0:
+                if 7 >= bit >= 0:
+                    data = self.client.ab_read(byte, 1)
+                    return int(s7util.get_bool(data, 0, bit))
+            return -1
+
+    def SetAI(self, startByte: int, value: int):
+        """
+        Set an analog input (AI) value as a 16-bit UNSIGNED INTEGER (0–65535) in the PLC input process image (E/I area).
+
+        byte: selects which input byte in the PLC is used, as defined by the GUI  
+        value: 0–65535 (word)
+        """
+        if self.isConnected():
+            buffer_AI = bytearray(2)
+            if startByte >= 0:
+                if 0 <= value <= 65535:
+                    if type(value) is float:
+                        lowByte = int(round(value)) & 0xFF  # 0xFF = mask for one byte (0b11111111)
+                        highByte = (int(round(value)) >> 8) & 0xFF
+
+                    elif type(value) is int:
+                        lowByte = value & 0xFF  # 0xFF = mask for one byte (0b11111111)
+                        highByte = (value >> 8) & 0xFF
+
+                    buffer_AI[0] = highByte
+                    buffer_AI[1] = lowByte
+                    self.client.eb_write(start=startByte, size=2, data=buffer_AI)
+                    return int(value)
+                return -1
+            return -1
+        
+    def GetAO(self,startByte: int):
+        """
+        Read an analog output (AO) value as a 16-bit SIGNED INTEGER (-32768–32767) from the PLC output process image (A/Q area).
+
+        byte: selects which output byte in the PLC is used, as defined by the GUI
+        """
+        if self.isConnected():
+            if startByte >= 0:
+                data = self.client.ab_read(start=startByte, size=2)
+                return s7util.get_int(data, 0)
+            return -1
+
+    def resetSendInputs(self, startByte: int, endByte: int):
+        """
+        Resets all send input data to the PLC (DI, AI)
+        """
+        if self.isConnected():
+            if startByte <= 0 and endByte > 0:
+                bufferEmpty = bytearray(2)
+                self.client.eb_write(start=startByte, size=(endByte - startByte), data=bufferEmpty)
+                self.client.ab_write(start=startByte, size=(endByte - startByte), data=bufferEmpty)
+                return True
+            else:
+                return False
+       
